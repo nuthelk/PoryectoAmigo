bd89077461090a5f39b01900c837e0012a9b9dfe
Felipe Jaramillo <Artimax85@gmail.com>
2022-11-11 17:03:21 -0500

    Editar perfil listo

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,38 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 import { Button } from "../components/Button";
+import { get, patchData } from "../helpers/crud";
+import { useForm } from "../Hooks/useForm";
 
 const Profile = () => {
+  const urlUsuarios = `https://mon-pays.fly.dev/usuarios/${"2"}`;
+
+  const [pintar, setPintar] = useState([]);
+
+  const { formValue, handleInputChangeName, reset } = useForm({
+    firstName: "",
+    lastName: "",
+    email:"",
+    address:""
+  });
+
+  useEffect(() => {
+    getItems().then((pintaritems) => setPintar(pintaritems));
+  }, []);
+
+
+  const getItems = async () => {
+    const data = await get(urlUsuarios)
+    return data;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const updateData = {
+      nombre:formValue.firstName,
+      apellido:formValue.lastName,
+      direccion:formValue.address,
+      Email:formValue.email,
+      password:pintar.password,
+      favoritos:pintar.favoritos,
+      id:pintar.id
+    }
+    Swal.fire({
+      title: 'Are you sure to edit?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, edit!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        patchData(urlUsuarios, updateData);
+        reset();
+        Swal.fire(
+          'Edited!',
+          'Your file has been Edited.',
+          'success'
+        )
+      }
+    })
+    
+    
+  }
+
   return (
     <div className="flex flex-col">
       <div className="m-auto mt-16 flex flex-col justify-center items-center gap-2">
         <img
           className="w-28 h-28 md:w-36 md:h-36 rounded-full"
-          src="https://scontent.feoh8-1.fna.fbcdn.net/v/t31.18172-8/12496322_1034668006592329_5690982609561500752_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=9267fe&_nc_eui2=AeEQzOHO6hJMjmdwJh4ksXWgL92zkxpNpTcv3bOTGk2lN7t71r6G5CvYjTE4UFmGqoHNXkUmtupt71FnOtNqhwva&_nc_ohc=hXkCksrKYxoAX8Wh4O3&_nc_ht=scontent.feoh8-1.fna&oh=00_AfCjuSOgAM-Xms34JnfnIOfBwa3WOhMzLxrBd01sjEcvSA&oe=63928984"
+          src="https://us.123rf.com/450wm/mrshamsjaman/mrshamsjaman2011/mrshamsjaman201100267/159314849-plantilla-de-dise%C3%B1o-de-vector-de-letra-inicial-mp-o-pm.jpg?ver=6"
           alt="photo"
         />
         <h1 className="font-bold text-xl md:text-2xl">My Profile</h1>
         <p className="md:text-lg">Update your profile</p>
       </div>
-      <form className="flex flex-col items-center mt-10 gap-5 px-10 mx-auto w-2/3 md:w-3/4 lg:w-1/2 lg:gap-6">
+      <form onSubmit={(e) => handleSubmit(e) } className="flex flex-col items-center mt-10 gap-5 px-10 mx-auto w-2/3 md:w-3/4 lg:w-1/2 lg:gap-6">
         <h1 className="font-bold text-xl md:text-2xl text-center">Basic Information</h1>
         <input
           className="md:text-lg md:h-12 h-10 border-2 pl-5 md:w-96 sm:w-72  "
-          placeholder="First Name"
+          onChange={handleInputChangeName}
+          name="firstName"
+          placeholder={pintar.nombre}
           type="text"
         />
         <input
           className="md:h-12 md:text-lg h-10 border-2 pl-5 md:w-96 sm:w-72 rounded-md "
-          placeholder="Last Name"
+          onChange={handleInputChangeName}
+          name="lastName"
+          placeholder={pintar.apellido}
           type="text"
         />
         <input
           className="md:h-12 md:text-lg h-10 border-2 pl-5 md:w-96 sm:w-72 rounded-md "
-          placeholder="Email@email.com"
+          onChange={handleInputChangeName}
+          name="email"
+          placeholder={pintar.Email}
           type="email"
         />
         <input
           className="md:h-12 md:text-lg h-10 border-2 pl-5 md:w-96 sm:w-72 rounded-md "
-          placeholder="Address"
+          onChange={handleInputChangeName}
+          name="address"
+          placeholder={pintar.direccion}
           type="text"
         />
         <div className="w-20 mx-auto md:w-32 mt-4 ">
