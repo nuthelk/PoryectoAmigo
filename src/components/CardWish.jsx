e5480436d76c6feb379331f8df0280f6d09930c8
Felipe Jaramillo <Artimax85@gmail.com>
2022-11-10 19:00:41 -0500

    Descripcion y Wish List Casi lista

--- src/components/CardWish.jsx
+++ src/components/CardWish.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
-import { Button } from './Button'
+import React, { useEffect, useState } from "react";
+import { Button } from "./Button";
 
 const CardWish = () => {
+  const urlFav = "https://proyecto-amigo.herokuapp.com/usuarios";
+  const [pintar, setPintar] = useState([]);
+
+  useEffect(() => {
+    getItems().then((pintaritems) => setPintar(pintaritems));
+  }, []);
+
+  const getItems = async () => {
+    const resp = await fetch(`${urlFav}/${"1"}`);
+    const data = await resp.json();
+
+    return data.favoritos;
+  };
+
+  const handlemesta = () => {
+    console.log(pintar);
+  };
+
   return (
     <>
-        <div className='flex flex-col items-center gap-5 justify-center'>
-            <h1 className='font-bold text-2xl mt-5'>Wish List</h1>
-            <div className='flex flex-col p-10 justify-center items-center md:items-start md:flex-row'>
-                <img className='w-2/3' src="https://i.pinimg.com/736x/21/28/4e/21284ec293da6c04197d6c9a1632157a.jpg" alt="list" />
-                <div className='flex flex-col text-center gap-5 p-5'>
-                    <h1 className='font-semibold text-xl'>Casa de mier** Literalmente esta hecha de eses</h1>
-                    <div className='h-24 overflow-y-scroll md:h-2/3 lg:h-44'>
-                        <p className='text-sm text-start'>Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion de la Descripcion</p>
-                    </div>
-                    <div className='w-1/2 mx-auto'>
-                        <Button text={"Get it"} />
-                    </div>
-                    
-                </div>
+      <div className="flex flex-col items-center gap-5 justify-center w-screen">
+        <h1 className="font-bold text-2xl mt-5">Wish List</h1>
+      </div>
+      {pintar.map(({nombre,descripcion,id,url}) => (
+        <div key={id} className="w-full border-b-2 border-b-gray-300 flex flex-col p-10 justify-center items-center md:items-start md:flex-row">
+          <img
+            className="w-1/3 place-self-center"
+            src={url.url1}
+            alt="list"
+          />
+          <div className="flex flex-col text-center gap-5 p-5">
+            <h1 className="font-semibold text-xl">
+              {nombre}
+            </h1>
+            <div className="h-24 overflow-y-scroll md:h-2/3 lg:h-44">
+              <p className="text-sm text-start">
+                {descripcion}
+              </p>
             </div>
+            <div onClick={handlemesta} className="w-1/3 mx-auto">
+              <button className="w-full bg-red-700 text-white h-10 rounded-md hover:bg-gray-400 transition-all duration-500 hover:border-transparent font-semibold">Delete</button>
+            </div>
+          </div>
         </div>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default CardWish
+export default CardWish;
--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -6,8 +6,9 @@ import { get, patchData, post } from "../helpers/crud";
 const Description = () => {
   const [addFav, setAddFav] = useState(false);
   const [urlsImg, setUrlsImg] = useState();
+  const [switche, setSwitche] = useState(false);
   const [pintar, setPintar] = useState([]);
-  const urlDescri = `https://proyecto-amigo.herokuapp.com/Propertys/${"2"}`;
+  const urlDescri = `https://proyecto-amigo.herokuapp.com/Propertys/${"5"}`;
   const urlFav = "https://proyecto-amigo.herokuapp.com/usuarios";
 
   useEffect(() => {
@@ -18,6 +19,13 @@ const Description = () => {
     const resp = await fetch(urlDescri);
     const data = await resp.json();
     setUrlsImg(data.url);
+    const {favoritos} = await get(`${urlFav}/${"1"}`)
+    const findFav = favoritos.find(e => e.id == data.id )
+    if(findFav){
+      setAddFav(true)
+    }else{
+      setAddFav(false)
+    }
 
     return data;
   };
@@ -25,32 +33,20 @@ const Description = () => {
   const handleWishList = async() => {
     
     const getFav = await get(`${urlFav}/${"1"}`)
-
     const {favoritos} = getFav
-    
+    const aux = favoritos
     const findFav = favoritos.find(e => e.id == pintar.id )
-    const datanueva = {}
-
-    if(findFav){
-      console.log("a");
-      patchData(`${urlFav}/${pintar.id}`, {favoritos:datanueva})
-      setAddFav(true)
-    }else{
-      patchData(`${urlFav}}`, {favoritos:pintar});
-      setAddFav(false)
-    }
     
-    /* const getFav = await get(`${urlFav}`)
-    const findFav = getFav.find(e => e.id == pintar.id )
-    const datanueva = {}
     if(findFav){
-      console.log("a");
-      patchData(`${urlFav}/${pintar.id}`, datanueva)
-      setAddFav(true)
-    }else{
-      post(`${urlFav}`, pintar);
+      const filtrarObj =favoritos.filter(e => e.id !== pintar.id)
+      patchData(`${urlFav}/${"1"}`, {favoritos:filtrarObj})
       setAddFav(false)
-    } */
+    }else{
+      aux.push(pintar)
+      console.log(aux);
+      patchData(`${urlFav}/${"1"}`, {favoritos:aux});
+      setAddFav(true)
+    }
 
   };
 
@@ -94,22 +90,7 @@ const Description = () => {
         </div>
         <div className="h-64 md:h-36 2xl:h-80 overflow-y-scroll md:overflow-auto">
           <p className="font-normal">
-            Revamp your style with the latest designer trends in men’s clothing
-            or achieve a perfectly curated wardrobe thanks to our line-up of
-            timeless pieces.Revamp your style with the latest designer trends in
-            men’s clothing or achieve a perfectly curated wardrobe thanks to our
-            line-up of timeless piecesRevamp your style with the latest designer
-            trends in men’s clothing or achieve a perfectly curated wardrobe
-            thanks to our line-up of timeless piecesRevamp your style with the
-            latest designer trends in men’s clothing or achieve a perfectly
-            curated wardrobe thanks to our line-up of timeless piecesRevamp your
-            style with the latest designer trends in men’s clothing or achieve a
-            perfectly curated wardrobe thanks to our line-up of timeless
-            piecesRevamp your style with the latest designer trends in men’s
-            clothing or achieve a perfectly curated wardrobe thanks to our
-            line-up of timeless piecesRevamp your style with the latest designer
-            trends in men’s clothing or achieve a perfectly curated wardrobe
-            thanks to our line-up of timeless pieces
+            {pintar.descripcion}
           </p>
         </div>
         <p className="font-semibold text-lg">Cartegoria:</p>
