118529bf72b409f8b8eb4c87074c5c3fc89106a8
Nuthelk <nuthelkcd@gmail.com>
2022-11-11 23:59:45 -0500

    pull felipe

--- src/pages/LayoutLogout.jsx
+++ src/pages/LayoutLogout.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import logo from '../img/logo.png'
 
 const LayoutLogout = () => {
-  
+
   return (
     <header className='bg-black h-16 '>
-        <div className='flex items-center text-white h-full  md:px-40 px-2 sm:px-8 justify-between'>
-          <Link to="/">
-            <h3 className=' text-2xl font-semibold cursor-pointer'>Mon-Pays</h3>
+      <div className='flex items-center text-white h-full  md:px-40 px-2 sm:px-8 justify-between'>
+        
+        <Link to="/">
+          <div className='flex items-center gap-4'>
+            <img src={logo} alt="logo" className='h-10' />
+            <h3 className='text-2xl font-semibold cursor-pointer w-28'>Mon-Pays</h3>
+          </div>
+        </Link>
+
+        <div className='flex justify-around items-center gap-3 sm:gap-8'>
+          <Link to="/loginEmail">
+            <p className='cursor-pointer text-lg'>Login</p>
           </Link>
-            
-            <div className='flex justify-around items-center gap-3 sm:gap-8'>
-              <Link to="/loginEmail">
-                <p className='cursor-pointer text-lg'>Login</p>
-              </Link>
-              <Link to="/registerEmail">
-                <p className='cursor-pointer text-lg'>Register</p>
-              </Link>
-            </div>
+          <Link to="/registerEmail">
+            <p className='cursor-pointer text-lg'>Register</p>
+          </Link>
         </div>
-        <Outlet />
+      </div>
+      <Outlet /> 
     </header>
   )
 }
